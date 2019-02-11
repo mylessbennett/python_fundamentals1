54cf9867afb398fccdd42c748d2da9ef0d6ffd98
Myles Bennett <myles.bennett@hotmail.com>
2019-02-11 16:00:25 -0500

    exercise 3 complete

--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,6 @@
-print("What's your name?")
+name = input("What's your name?")
+current_year = 2019
+user_age = current_year - int(input("What year were you born in?"))
+
+
+print("{}, your age is {}".format(name, user_age))
