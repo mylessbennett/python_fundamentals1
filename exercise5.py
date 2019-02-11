61d860be3e432fc810bec11f2091514a34559068
Myles Bennett <myles.bennett@hotmail.com>
2019-02-11 17:05:34 -0500

    completed exercise 6.1

--- exercise5.py
+++ exercise5.py
@@ -1,12 +1,15 @@
-move = input("Would you like to walk or run? (s to stop) ")
+move = input("Would you like to walk or run? (enter 'go home' to stop) ")
 distance = 0
 
 while move == "walk" or move == "run":
     if move == "walk":
         distance += 1
+        print("You are {}km from home".format(distance))
         move = input("Would you like to walk or run? ")
     elif move == "run":
         distance += 5
+        print("You are {}km from home".format(distance))
         move = input("Would you like to walk or run? ")
-
-    print("You are {}km from home".format(distance))
+    elif move == "go home":
+        print("You are {}km from home".format(distance))
+        print("Time to head home!")
