e25d88f93a0553b7480cb8b9a60b9bbcf4092fd3
Myles Bennett <myles.bennett@hotmail.com>
2019-02-11 14:30:11 -0500

    exercise 2

---
+++ exercise2.py
@@ -0,0 +1,12 @@
+#1. Multiply the cost of the meal by the tip percentage and then add it to cost.
+print(55 + 55 * 0.2)
+
+#2. Convert the string to and integer or float using int() or float() and then add.
+print(int("42") + 42)
+print(float("42") + 42)
+
+#3. Use .format() and {}.
+print("45628 x 7839 = {}".format(45628 * 7839))
+
+#4. ((True and False) or not(False)) >>> (False) or True >>> True
+print((10 < 20 and 30 < 20) or not(10 == 11))
